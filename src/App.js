7b103e85c987c2de7f34f06359b6233bfe9d2dd7
EsviCodes <esther.js.vis@gmail.com>
2019-10-04 15:17:51 +0200

    Fetched albums

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import AlbumsListContainer from "./components/AlbumsListContainer";
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <div className="App">
           <h1>Photo Album</h1>
+          <AlbumsListContainer />
         </div>
       </Provider>
     );
